620edbdaf711d61ae3d737ea9f7ef30fa59bd7dd
RAJAN <75211405+rajan-shende@users.noreply.github.com>
2024-10-27 14:49:37 +0530

    Add files via upload

--- GCP_INFRA_PROJECT/main.tf
+++ GCP_INFRA_PROJECT/main.tf
@@ -1,4 +1,9 @@
-resource "google_storage_bucket" "infra-bucket-v1"{
-    name = "infra-bucket-v1"
+#  Cloud storage implementation for deployment architecture. 
+#  Maintaining remote state 
+
+resource "google_storage_bucket" "infra-state-bucket-v1"{
+    project = "RAJAN-TERRAFORM-GCP" 
+    name = "state-bucket-v1"
     location = "us-east1"
+    
 }
